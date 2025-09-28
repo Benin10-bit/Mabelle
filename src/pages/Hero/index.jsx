a1fcbaa6e06902142ad770bc1266840972b40d2c
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2025-09-27 23:22:40 -0300

    ajuste sla das quantas

--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -2,7 +2,7 @@ import hero from "../../assets/hero.jpeg";
 
 export default function Hero() {
   return (
-    <div className="h-[110vh] bg-[#F5FFFF] text-[#4fbbbe] font-bold flex-col flex justify-center items-center">
+    <div className="h-[100vh] bg-[#F5FFFF] text-[#4fbbbe] font-bold flex-col flex justify-center items-center">
       <img src={hero} alt="hero da página" />
       <p className="text-xl mb-16 text-center">
         A beleza que você merece, em cada detalhe.
